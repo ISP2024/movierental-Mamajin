4c54fe740133873e1a1a58a37f2e75e3a014386b
Phasit Ruangmak <phasit2005@gmail.com>
2024-10-14 20:50:03 +0700

    Move get_price Method to Rental and added new unittests

--- rental_test.py
+++ rental_test.py
@@ -18,14 +18,15 @@ class RentalTest(unittest.TestCase):
         self.assertEqual("Air", m.get_title())
         self.assertEqual(Movie.REGULAR, m.get_price_code())
 
-    @unittest.skip("add this test when you refactor rental price")
     def test_rental_price(self):
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.get_price(), 3.0)
         rental = Rental(self.new_movie, 5)
         self.assertEqual(rental.get_price(), 15.0)
-        self.fail("TODO add more tests for other movie categories")
+        rental = Rental(self.new_movie, 2)
+        self.assertEqual(rental.get_price(), 1.50)
 
     @unittest.skip("add this test of rental points when you add it to Rental")
     def test_rental_points(self):
         self.fail("add this test of frequent renter points")
+
